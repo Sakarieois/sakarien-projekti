d73561c9673ec26027037c9d034f44ef21396778
Sakarie Jamac <sj0695@edu.turku.fi>
2025-05-08 13:29:59 +0300

    moi

---
+++ henkilötiedot.js
@@ -0,0 +1,65 @@
+const henkilot = [
+    {
+        name: "Merja Meikäläinen",
+        age: 20,
+        job: "Opiskelija",
+        driversLicense: true
+    },
+    {
+        name: "Teppo Teikäläinen",
+        age: 25,
+        job: "Sähköteknikko",
+        driversLicense: true
+    },
+    {
+        name: "Helena Heikäläinen",
+        age: 30,
+        job: "Ohjelmistosuunnittelija",
+        driversLicense: false
+    },
+    {
+        name: "Semir Sikäläinen",
+        age: 18,
+        job: "Opiskelija",
+        driversLicense: true
+    },
+    {
+        name: "Tomas Täkäläinen",
+        age: 22,
+        job: "Linja-autonkuljettaja",
+        driversLicense: true
+    }
+];
+
+window.onload = function() {
+
+    const rivit = document.getElementById("rivit");
+    
+
+    function luoRivit() {
+        rivit.innerHTML = ""; 
+        henkilot.forEach(function(henkilo) {
+            const rivi = document.createElement("tr");
+            
+            const nimiTd = document.createElement("td");
+            nimiTd.innerHTML = henkilo.name;
+            rivi.appendChild(nimiTd);
+            
+            const ikaTd = document.createElement("td");
+            ikaTd.innerHTML = henkilo.age;
+            rivi.appendChild(ikaTd);
+            
+            const työTd = document.createElement("td");
+            työTd.innerHTML = henkilo.job;
+            rivi.appendChild(työTd);
+            
+            const ajokorttiTd = document.createElement("td");
+            ajokorttiTd.innerHTML = henkilo.driversLicense ? "Kyllä" : "Ei";
+            rivi.appendChild(ajokorttiTd);
+            
+            rivit.appendChild(rivi);
+        });
+    }
+    
+    luoRivit(); 
+}
